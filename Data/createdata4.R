7c9d04ba45475b85235fb79d2307a42b447a70d4
Annina Haverinen <annina.haverinen@helsinki.fi>
2018-11-24 12:52:35 +0200

    Update Data/createdata4.R

---
+++ Data/createdata4.R
@@ -0,0 +1,22 @@
+#loading the MASS dataset already loaded in R
+library(MASS)
+data("Boston")
+str(Boston)
+dim(Boston) #506 observation of 14 variables
+head(Boston)
+colnames(Boston)
+# link [https://stat.ethz.ch/R-manual/R-devel/library/MASS/html/Boston.html]
+#Housing values in suburbs of Boston
+# crim (per capita crime rate by town), zn (proportion of residential land zoned for lots over 25,000 sq.ft),
+#indus (proportion of non-retail business acres per town)
+#chas, Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+#Nox, nitrogen oxides concentration (parts per 10 million).
+#rm, average number of rooms per dwelling.
+#age, proportion of owner-occupied units built prior to 1940.
+#dis, weighted mean of distances to five Boston employment centres.
+#rad,index of accessibility to radial highways.
+#tax,full-value property-tax rate per \$10,000.
+#ptratio,pupil-teacher ratio by town.
+#black, 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
+#lstat, lower status of the population (percent).
+# medv,median value of owner-occupied homes in \$1000s.
